9018dea3cbee4b2d3ab45a4582aa44e32dbf9381
JakubJamarik <103573874+JakubJamarik@users.noreply.github.com>
2023-05-01 11:43:43 +0200

    Corrected some typos
    line 26 -- Changed "patches" to "parts" so it matches the rest of the scripts.
    line 31 -- Changed "Fles" to "Files" as it was incorrect.

--- scripts/set_AveCProf_env.sh
+++ scripts/set_AveCProf_env.sh
@@ -23,12 +23,12 @@ export AVECPROF_BIN_SAMPLECORTEX="${AVECPROF}/C++/profilesampling/samplecortex"
 
 export AVECPROF_R_COMPUTEHISTBOUNDARIES="${AVECPROF}/R/computeHistBoundaries.R"
 # bash scripts
-export AVECPROF_SH_PERFORMSURFACESAMPLING="${AVECPROF}/scripts/perform_surface_sampling_in_patches.sh"
+export AVECPROF_SH_PERFORMSURFACESAMPLING="${AVECPROF}/scripts/perform_surface_sampling_in_parts.sh"
 export AVECPROF_SH_CREATESURFACEMEASUREMENTS="${AVECPROF}/scripts/create_subject_surface_measurements.sh"
 
 # R scripts
 export AVECPROF_R_OPTIMIZEPROFILES="${AVECPROF}/R/optimizeProfiles.R"
-export AVECPROF_R_PREPRAWCORTEXFILES="${AVECPROF}/R/prepRawCortexFles.R"
+export AVECPROF_R_PREPRAWCORTEXFILES="${AVECPROF}/R/prepRawCortexFiles.R"
 export AVECPROF_R_COMPUTEHISTBOUNDARIES="${AVECPROF}/R/computeHistBoundaries.R"
 
 #
